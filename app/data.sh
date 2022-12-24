e2aef6a22ec722b27d7637d7791af55cdfc3b759
jessepereira <jessepereira_@hotmail.com>
2022-12-24 06:07:09 +0000

    ajuste no script

--- app/data.sh
+++ app/data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #touch `date +%d-%b-%Y-%H`h.txt
 data=$(date +"%H")
-echo "$data" > /home/jessepereira02/Builder/app/files/`date +%d-%b-%Y-%H`h.txt
+echo "$data" > /home/jessepereira02/Builder/app/files/`date +%M:%S-%d-%b-%Y-%H`h.txt
 
-gcloud storage cp /home/jessepereira02/Builder/app/files/`date +%d-%b-%Y-%H`h.txt gs://36eac891354952e8-bucket-tfstate
+gcloud storage cp /home/jessepereira02/Builder/app/files/`date +%M:%S-%d-%b-%Y-%H`h.txt gs://36eac891354952e8-bucket-tfstate
